9fc2a80cce59edcd901f6c1d9ec37ab2fc6a92cd
ynom <nomatter@domain.com>
2024-11-22 14:38:42 -0800

    Example finished

--- set-getTests/set_getTests.swift
+++ set-getTests/set_getTests.swift
@@ -27,4 +27,41 @@ struct set_getTests {
         #expect(Money(cents: 233).dollars == MoneyWithAFunction(cents: 233).dollars())
     }
 
+    
+}
+
+struct rect_Tests {
+    struct Point {
+        var x = 0.0, y = 0.0
+    }
+    struct Size {
+        var width = 0.0, height = 0.0
+    }
+    struct Rect {
+        var origin = Point()
+        var size = Size()
+        var center: Point {
+            get {
+                let centerX = origin.x + (size.width / 2)
+                let centerY = origin.y + (size.height / 2)
+                return Point(x: centerX, y: centerY)
+            }
+            set(newCenter) {
+                origin.x = newCenter.x - (size.width / 2)
+                origin.y = newCenter.y - (size.height / 2)
+            }
+        }
+    }
+    
+    
+    @Test func getSetTests() async throws {
+        var square = Rect(origin: Point(x: 0.0, y: 0.0),
+                          size: Size(width: 10.0, height: 10.0))
+        #expect(square.center.x == 5)
+        #expect(square.center.y == 5)
+        
+        square.center = Point(x: 15.0, y: 15.0)
+        #expect(square.origin.x == 10)
+        #expect(square.origin.y == 10)
+    }
 }
